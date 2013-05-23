af6fe8405b723062778bf5a94190575e77474060
unknown <dog_champ@hotmail.com>
2013-05-24 02:20:08 +0700

    load s1 s2 s3

--- littlefield_calc/excel/excel_ctrl.py
+++ littlefield_calc/excel/excel_ctrl.py
@@ -1,3 +1,32 @@
 import os
+import csv
 
 current_directory = os.getcwd()
+
+
+class DataController():
+    def _load_array_of_content(self, filename):
+        content = []
+        with open(filename, 'r') as station_machine:
+            c = csv.reader(station_machine, delimiter='\t', skipinitialspace=True)
+            for line in c:
+                content.append(line)
+        return content
+
+    station_1_queue_contents = _load_array_of_content([], 'd:/test/s1q.xls')
+    station_1_utilization_contents = _load_array_of_content([], 'd:/test/s1u.xls')
+    station_2_queue_contents = _load_array_of_content([], 'd:/test/s2q.xls')
+    station_2_utilization_contents = _load_array_of_content([], 'd:/test/s2u.xls')
+    station_3_queue_contents = _load_array_of_content([], 'd:/test/s3q.xls')
+    station_3_utilization_contents = _load_array_of_content([], 'd:/test/s3u.xls')
+
+    with open('d:/test/summary.xls', 'w') as little_field_summary:
+        for index in range(len(station_1_queue_contents)):
+            little_field_summary.write(station_1_queue_contents[index][0])
+            little_field_summary.write('\t')
+            little_field_summary.write(station_1_queue_contents[index][1])
+            
+            little_field_summary.write('\n')
+
+
+
